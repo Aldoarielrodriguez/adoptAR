87a9ec9e1306ae83e812735816bf76749e719857
Aldoarielrodriguez <Aldoarielro@gmail.com>
2023-05-08 10:50:53 -0300

    Create splitter.js

---
+++ js/splitter.js
@@ -0,0 +1,31 @@
+var Splitter= 
+{
+
+    onReady: function (){
+
+        var left = document.querySelector(".left");
+        var right = document.querySelector(".right");
+        var container = document.querySelector(".especieContainer");
+
+
+        left.addEventListener("mouseenter",()=>{
+            container.classList.add("hover-left");
+        })
+
+        left.addEventListener("mouseleave",()=>{
+            container.classList.remove("hover-left");
+        })
+
+        right.addEventListener("mouseenter",()=>{
+            container.classList.add("hover-right");
+        })
+
+        right.addEventListener("mouseleave",()=>{
+            container.classList.remove("hover-right");
+        })
+
+       
+    }
+}
+
+$('document').ready(Splitter.onReady);
